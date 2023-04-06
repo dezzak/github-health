1e5e4f5dccc65e886926cc540431daf5b89cf57b
Derek Kaye <github@dezzanet.co.uk>
2023-04-06 16:25:18 +0100

    🧹 run prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,59 @@
 module.exports = {
-    // Prevent ESLint from using rules in parent folders to this project.
-    root: true,
-    extends: [
-        'eslint:recommended',
-        'plugin:import/recommended',
-        "plugin:react/recommended",
-        // Airbnb's React ruleset, adapted for TypeScript
-        'airbnb-typescript',
-        // Prettier rules and plugins. Must be included after other extends, to avoid conflict.
-        'plugin:prettier/recommended',
+  // Prevent ESLint from using rules in parent folders to this project.
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    // Airbnb's React ruleset, adapted for TypeScript
+    'airbnb-typescript',
+    // Prettier rules and plugins. Must be included after other extends, to avoid conflict.
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['/*.js', 'stubs/*', 'dist/*', 'node_modules/*'],
+  rules: {
+    'no-process-env': 'off',
+
+    'no-console': 'off',
+
+    // TypeScript is providing compile time prop checking
+    'react/prop-types': 'off',
+
+    'react/require-default-props': 'off',
+
+    // Enforce import ordering and spacing
+    'import/order': ['error', { 'newlines-between': 'always' }],
+
+    // disallows irregular whitespace that can't easily be seen
+    'no-irregular-whitespace': [
+      'error',
+      {
+        skipStrings: false,
+        skipTemplates: false,
+        skipRegExps: false,
+        skipComments: false,
+      },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json',
+  },
+  overrides: [
+    {
+      files: ['src/**/*.{ts,tsx}'],
     },
-    env: {
-        node: true,
-        browser: true,
-        es6: true,
-        jest: true,
+  ],
+  settings: {
+    react: {
+      version: '16.0',
     },
-    plugins: ['@typescript-eslint'],
-    ignorePatterns: ['/*.js', 'stubs/*', 'dist/*', 'node_modules/*'],
-    rules: {
-        'no-process-env': 'off',
-
-        'no-console': 'off',
-
-        // TypeScript is providing compile time prop checking
-        'react/prop-types': 'off',
-
-        'react/require-default-props': 'off',
-
-        // Enforce import ordering and spacing
-        'import/order': ['error', { 'newlines-between': 'always' }],
-
-        // disallows irregular whitespace that can't easily be seen
-        'no-irregular-whitespace': [
-            'error',
-            {
-                skipStrings: false,
-                skipTemplates: false,
-                skipRegExps: false,
-                skipComments: false,
-            },
-        ],
-    },
-    overrides: [
-        {
-            files: ['src/**/*.{ts,tsx}']
-        }
-    ],
-    settings: {
-        react: {
-            version: '16.0',
-        },
-    },
+  },
 }
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # GitHub Health
 
 Personal development project to read github repositories and check things such as
+
 - Dependabot config
 - Codeowners set
 - PR template set
